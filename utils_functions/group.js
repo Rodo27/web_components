0b05c248f1728d75629f69bd92834198a36ecbb3
Aldo Rodrigom <aldo1rodrigo@gmail.com>
2023-07-24 16:46:47 -0600

    add resources and folder utils

---
+++ utils_functions/group.js
@@ -0,0 +1,23 @@
+
+const array = [1,2,3,4,5]
+
+// Object.groupBy  groups items by arbitrary key
+// In this case, we´re grouping by even/odd keys 
+
+Object.groupBy(array, (num, index) =>  { //Object.groupBy is not a function
+    return num % 2 === 0 ? "even" : "odd"
+})
+
+// => { ood: [1,3,5], even: [2,4]}
+
+// Map.groupBy returns items in a Map, and is useful for grouping
+// using an object key
+
+const odd = {odd: true}
+const even = {even: true}
+
+Map.groupBy(array, (num, index) => {
+    return num % 2 === 0 ? even : odd
+})
+
+// => Map { {odd: true}: [1,3,5], {even: true}: [2,4] }
